d4aaa47f08c6d280aa625e1c2c5345d2035c9d46
Emanuel Turis <hello@emanuelturis.com>
2022-04-17 13:58:07 +0100

    Add posts HTML with data from Ghost CMS

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,7 +33,34 @@ const Home: NextPage<Props> = ({ posts }) => {
       </Head>
 
       <main>
-        <div></div>
+        <div className="max-w-screen-lg mx-auto mt-24">
+          <h1 className="text-5xl text-gray-900 font-bold">Posts</h1>
+          <p className="text-gray-700 mt-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+            eros sem, semper vitae molestie sed, mattis at risus. Suspendisse
+            vitae convallis arcu, at laoreet felis. Fusce in nunc et sapien
+            tincidunt aliquam. Curabitur ornare blandit dolor ut commodo. Donec
+            sollicitudin quam leo, vitae ullamcorper dolor condimentum sit amet.
+          </p>
+          <div className="grid grid-cols-2 gap-5 mt-8">
+            {posts.map((post) => (
+              <div
+                key={post.id}
+                className="border border-gray-100 rounded-md px-7 py-4 shadow-sm flex flex-col items-start"
+              >
+                <h2 className="text-2xl font-semibold text-gray-900">
+                  {post.title}
+                </h2>
+                <p className="line-clamp-3 mt-2 mb-8 text-gray-700">
+                  {post.excerpt}
+                </p>
+                <button className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-md mt-auto">
+                  Read More
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
       </main>
     </div>
   );
